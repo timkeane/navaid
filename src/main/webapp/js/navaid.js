031a7e0d88138b753a00813403e60b8e9d3286fd
timkeane <tkeane@doitt.nyc.gov>
2017-09-21 19:00:39 -0400

    make dialog a member

--- src/main/webapp/js/navaid.js
+++ src/main/webapp/js/navaid.js
@@ -19,6 +19,7 @@ tk.NavAid = function(options){
   this.popup = new nyc.ol.Popup(this.map);
   this.on(nyc.ol.Tracker.EventType.UPDATED, this.updateCurrentTrack, this);
   this.source = new ol.source.Vector();
+  this.dia = new nyc.Dialog();
   this.baseLayer();
   this.initDraw(this.source);
   this.restoreFeatures();
@@ -488,7 +489,7 @@ tk.NavAid.prototype = {
   toggleNav: function(){
     var me = this, btn = me.navBtn;
     if (btn.hasClass('stop')){
-      new nyc.Dialog().yesNo({
+      me.dia.yesNo({
         message: 'Stop navigation?',
         callback: function(yesNo){
           if (yesNo){
@@ -730,7 +731,7 @@ tk.NavAid.prototype = {
    */
   nameFeature: function(feature){
     var me = this;
-    new nyc.Dialog().input({
+    me.dia.input({
       placeholder: 'Enter a name...',
       callback: function(name){
         if (!name){
@@ -739,7 +740,7 @@ tk.NavAid.prototype = {
           feature.setId(name);
           me.updateStorage();
         }else{
-          new nyc.Dialog().ok({
+          me.dia.ok({
               message: '<b>' + name + '</b> is already assigned',
               callback: function(){
                 me.nameFeature(feature);
@@ -758,7 +759,7 @@ tk.NavAid.prototype = {
     var me = this,
       target = $(event.target),
       feature = target.data('feature');
-    new nyc.Dialog().yesNo({
+    me.dia.yesNo({
       message: 'Delete <b>' + feature.getId() + '</b>?',
       callback: function(yesNo){
         if (yesNo){
@@ -784,8 +785,7 @@ tk.NavAid.prototype = {
   importExport: function(event){
     var me = this, storage = me.storage, btn = $(event.target);
     if (btn.hasClass('empty')){
-      var dia = new nyc.Dialog()
-      dia.yesNo({
+      me.dia.yesNo({
         message: 'Delete all location data?',
         callback: function(yesNo){
           if (yesNo){
